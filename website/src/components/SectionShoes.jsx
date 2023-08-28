a1bdf6370187df68e64cea863d56e603cf875fe6
Yefree Valdez <yefree13santos@outlook.com>
2023-08-28 23:58:36 +0200

    feat: create components

---
+++ website/src/components/SectionShoes.jsx
@@ -0,0 +1,41 @@
+export default function SectionShoes() {
+  return (
+    <section>
+      <h2>Always Iconic</h2>
+      <div className="flex flex-nowrap">
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+        <article>
+          <img src="/Images/iconic/shoes.webp" alt="" />
+          <h3>Air Jordan 1</h3>
+        </article>
+      </div>
+    </section>
+  );
+}
