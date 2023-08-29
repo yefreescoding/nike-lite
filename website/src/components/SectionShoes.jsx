3c5c8a90f8e21d03777f952ec210a031c61e3eec
Yefree Valdez <yefree13santos@outlook.com>
2023-08-29 14:32:09 +0200

    feat: react component scroll shoes

--- website/src/components/SectionShoes.jsx
+++ website/src/components/SectionShoes.jsx
@@ -1,40 +1,90 @@
+// Library imports
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
+
+// react imports
+import { useRef } from 'react';
+
 export default function SectionShoes() {
+  const containerRef = useRef(null);
+
+  const handleClick = (e) => {
+    const direction = e.target.dataset.direction;
+    sideScroll(containerRef.current, direction, 25, 200, 10);
+    console.log(direction);
+  };
+
+  function sideScroll(element, direction, speed, distance, step) {
+    let scrollAmount = 0;
+
+    const slideTimer = setInterval(function () {
+      if (direction === 'left') {
+        element.scrollLeft -= step;
+      } else {
+        element.scrollLeft += step;
+      }
+      scrollAmount += step;
+      if (scrollAmount >= distance) {
+        clearInterval(slideTimer);
+      }
+    }, speed);
+  }
   return (
     <section>
-      <h2>Always Iconic</h2>
-      <div className="flex flex-nowrap">
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
-        <article>
-          <img src="/Images/iconic/shoes.webp" alt="" />
-          <h3>Air Jordan 1</h3>
-        </article>
+      <h2 className="text-2xl font-bold mb-8">Always iconic</h2>
+      <div className="relative bg-blue-200">
+        <button
+          data-direction="left"
+          onClick={handleClick}
+          className="z-[9999] absolute left-10 top-[40%] shadow-lg p-4 rounded-full bg-gray-100"
+        >
+          <ChevronLeftIcon className="h-6 w-6 text-blue-800" />
+        </button>
+        <div
+          id="container"
+          ref={containerRef}
+          className="relative overflow-scroll p-4 grid gap-4 grid-flow-col auto-cols-[35%] lg:auto-cols-[20%] snap-x-mandatory"
+        >
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+          <article className="snap-center">
+            <img src="/Images/iconic/shoes.webp" alt="" />
+            <h3>Air Jordan 1</h3>
+          </article>
+        </div>
+        <button
+          data-direction="right"
+          onClick={handleClick}
+          className="z-[9999] absolute right-10 top-[40%] shadow-lg p-4 rounded-full bg-gray-100"
+        >
+          <ChevronRightIcon className="h-6 w-6 text-blue-800 " />
+        </button>
       </div>
     </section>
   );
